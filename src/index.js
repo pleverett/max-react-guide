7105add1e5c5339f2922a5a0ab5d7a26fe503279
Paul Leverett <pleverett@gmail.com>
2018-02-12 15:08:52 -0800

    Finished module 4

--- src/index.js
+++ src/index.js
@@ -10,31 +10,35 @@ const styles = {
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 28 }
+      { id: 'ak3eke', name: 'Max', age: 28 },
+      { id: 'adrefe', name: 'Manu', age: 29 },
+      { id: 'cvaree', name: 'Stephanie', age: 28 }
     ],
     showPersons: false
   }
 
-  switchNameHandler = newName => {
-    // console.log("was clicked")
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    })
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1)
+    this.setState({persons: persons})
   }
 
-  nameChangedHandler = event => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(person => {
+      return person.id === id
+    })
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value
+
+    const persons = [...this.state.persons]
+    persons[personIndex] = person
+
     this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
+      persons: persons
     })
   }
 
@@ -56,8 +60,13 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            ;<Person name={person.name} age={person.age} />
+          {this.state.persons.map((person, index) => {
+            return <Person 
+            click={() => this.deletePersonHandler(index)}
+            name={person.name} 
+            age={person.age} 
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div>
       )
